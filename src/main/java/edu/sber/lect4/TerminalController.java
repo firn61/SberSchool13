448daead23066a68fa9620b093abee72f7462097
firn61 <firn@list.ru>
2021-08-27 16:06:40 +0300

    hw 4 p1

---
+++ src/main/java/edu/sber/lect4/TerminalController.java
@@ -0,0 +1,18 @@
+package edu.sber.lect4;
+
+public class TerminalController {
+
+    private PinValidator pinValidator;
+    private TerminalView terminalView;
+
+    TerminalController(PinValidator pinValidator, TerminalView terminalView){
+        this.pinValidator=pinValidator;
+        this.terminalView=terminalView;
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/edu/sber/lect4/TerminalServer.java
@@ -0,0 +1,10 @@
+package edu.sber.lect4;
+
+public class TerminalServer {
+
+    public static void main(String[] args) {
+        TerminalView fv = new TerminalView();
+        fv.initFrame();
+    }
+
+}
